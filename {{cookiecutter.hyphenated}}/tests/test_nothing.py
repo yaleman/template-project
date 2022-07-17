e7ab5b11a49c16434f788917baba8f368d06406a
James Hodgkinson <james@terminaloutcomes.com>
2022-07-17 10:12:13 +1000

    tweaks to basic template

---
+++ {{cookiecutter.hyphenated}}/tests/test_nothing.py
@@ -0,0 +1,8 @@
+""" testing code which doesn't test anything """
+
+# from {{ cookiecutter.underscored }} import example_function
+
+
+def test_nothing() -> None:
+    """ doesn't test anything """
+    assert example_function() == 2
